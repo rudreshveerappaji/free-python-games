8487cb3d1137926864d22ad48c4c1e3dd48c399a
rudreshveerappaji <v.rudresh@gmail.com>
2021-06-04 20:52:16 -0400

    Guess game - attempts included

--- freegames/guess.py
+++ freegames/guess.py
@@ -14,6 +14,7 @@ from random import randint
 start = 1
 end = 100
 value = randint(start, end)
+count = 0
 
 print(value)
 print("I'm thinking of a number between", start, "and", end)
@@ -21,6 +22,7 @@ print("I'm thinking of a number between", start, "and", end)
 guess = None
 
 while guess != value:
+    count = count+1
     text = input("Guess the number: ")
     guess = int(text)
 
@@ -29,4 +31,4 @@ while guess != value:
     elif guess > value:
         print("Lower.")
 
-print("Congratulations! You guessed the right answer:", value)
+print("Congratulations! You guessed the right answer", value,"in", count,"attempts.!")
